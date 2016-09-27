26bf085fb77a9fc8315063509d38829813f8f688
13dec2537 <13dec2537@gmail.com>
2016-09-27 14:11:20 +0700

    Revert "อัพเดรต"
    This reverts commit 7aa11945e5812d55679f8ed06388516a812255a6.

--- LionMonitoringDemo/app/src/main/java/com/example/kuybeer26092016/lionmonitoringdemo/MainActivity.java
+++ LionMonitoringDemo/app/src/main/java/com/example/kuybeer26092016/lionmonitoringdemo/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.kuybeer26092016.lionmonitoringdemo;
 
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -24,6 +22,16 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -72,32 +80,22 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_tower2) {
-            FragTower2();
-        } else if (id == R.id.nav_dk100) {
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_me) {
+        } else if (id == R.id.nav_slideshow) {
 
-        } else if (id == R.id.nav_logout) {
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
 
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
-    }
-    public void FragTower2(){
-        FragmentTower2 fragtw2 = new FragmentTower2();
-        android.support.v4.app.FragmentManager fm = getSupportFragmentManager();
-        android.support.v4.app.FragmentTransaction ft = fm.beginTransaction();
-        ft.replace(R.id.MainlayoutFragments,fragtw2);
-        ft.commit();
-    }
-    public void FragDk100(){
-        FragmentTower2 fragtw2 = new FragmentTower2();
-        android.support.v4.app.FragmentManager fm = getSupportFragmentManager();
-        android.support.v4.app.FragmentTransaction ft = fm.beginTransaction();
-        ft.replace(R.id.MainlayoutFragments,fragtw2);
-        ft.commit();
     }
 }
