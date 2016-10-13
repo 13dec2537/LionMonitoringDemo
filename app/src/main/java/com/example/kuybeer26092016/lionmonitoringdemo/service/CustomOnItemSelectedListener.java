20e3e36641512331a705763c83e79c1172718a1f
13dec2537 <13dec2537@gmail.com>
2016-10-13 20:33:26 +0700

    อัพเดรต

---
+++ app/src/main/java/com/example/kuybeer26092016/lionmonitoringdemo/service/CustomOnItemSelectedListener.java
@@ -0,0 +1,19 @@
+package com.example.kuybeer26092016.lionmonitoringdemo.service;
+
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
+
+/**
+ * Created by KuyBeer26092016 on 6/10/2559.
+ */
+public class CustomOnItemSelectedListener implements android.widget.AdapterView.OnItemSelectedListener {
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
